e39755813941e0452d392123e8dffadfa281a186
Helen Murugan <gallonhelen@hotmail.com>
2023-05-13 19:51:58 +0000

    Fix errors in division function and event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     document.getElementById("answer-box").addEventListener("keydown", function(event) {
-        if (event === "Enter") {
-            checkAnswer();
-        }
-    });
-
+		if (event.key === "Enter") {
+			checkAnswer();
+		}
+	});
+    
     runGame("addition");
 });
 
@@ -36,6 +36,7 @@ function runGame(gameType) {
     //creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    let num3 = num1 * num2;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -43,12 +44,8 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-
     } else if (gameType === "division") {
-            displayDivisionQuestion(num1, num2); {      
-            document.getElementById("operand1").textContent = (operand1 * operand2);      
-            document.getElementById("operand2").textContent = operand2;}
-
+            displayDivisionQuestion(num3, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
